b73b0a044d61f6e124f83234a5e2434c14344f2d
jhjeong <kkiu1756@gmail.com>
2021-08-11 20:03:04 +0900

    Posts req,res dto 생성

---
+++ src/main/java/com/jhjeong/restapi/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.jhjeong.restapi.web.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+  private Long id;
+  private String title;
+  private String content;
+  private String author;
+
+  public PostsResponseDto(Long id, String title, String content, String author) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.author = author;
+  }
+}
